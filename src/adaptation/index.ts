789a6c3566737bb3776538154c5d581e6611cbd9
Charles Castillo <mrcodeislife718@proton.me>
2026-08-09 00:31:37 -0400

    Export universal adaptation components

---
+++ src/adaptation/index.ts
@@ -0,0 +1,6 @@
+export * from "./EmbodimentContinuityManager.js";
+export * from "./CapabilityNegotiationEngine.js";
+export * from "./UncertaintyPropagation.js";
+export * from "./EdgeCaseAdaptationSupervisor.js";
+export * from "./AutomaticCalibrationPlanner.js";
+export * from "./HotSwapCoordinator.js";
